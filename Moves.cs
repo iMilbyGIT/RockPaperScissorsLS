ff93cb400ac3d209353e7d2fef6cc30c4a802eff
Adam Milbrath <adammilbrath@gmail.com>
2019-11-20 15:55:36 -0600

    decided to add another class for moves. I think I'll need that due to how much math in behind each of the moves.

---
+++ Moves.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpsls
+{
+    public class Moves
+    {
+        public string rock;
+        public string scissors;
+        public string paper;
+        public string lizard;
+        public string spock;
+
+        //    string[] allMoveOptions = new string[4];
+        //    allMoveOptions[0] = "Rock";
+        //    allMoveOptions[1] = "Scissors";
+        //    allMoveOptions[2] = "Paper";
+        //    allMoveOptions[3] = "Lizard";
+        //    allMoveOptions[4] = "Spock";
+
+    }
+}
